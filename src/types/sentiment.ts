2d8c65998ac0e82648670b46b275221d8bc9e5e7
michota <dawidmichota@gmail.com>
2025-02-11 17:07:02 +0100

    feat: Sentiment types

---
+++ src/types/sentiment.ts
@@ -0,0 +1,17 @@
+import { TextClassification } from "@/services/fetchSentimentAnalysis";
+
+export const Sentiment = {
+  POSITIVE: "POSITIVE",
+  NEUTRAL: "NEUTRAL",
+  NEGATIVE: "NEGATIVE",
+};
+
+export type Sentiment = keyof typeof Sentiment;
+
+export interface SentimentAnalysis extends TextClassification {
+  label: Sentiment;
+}
+
+export function isSentimentAnalysis(analysis: { label: string }): analysis is SentimentAnalysis {
+  return Object.values(Sentiment).includes(analysis.label);
+}
