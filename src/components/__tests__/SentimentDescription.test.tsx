ae6fbf155bdf7f756e94b6b976862b2fbadab09b
michota <dawidmichota@gmail.com>
2025-02-12 10:35:29 +0100

    feat: create basic tests for SentimentDescription

---
+++ src/components/__tests__/SentimentDescription.test.tsx
@@ -0,0 +1,6 @@
+describe("Render component", () => {
+  test("Render sentiment", () => {});
+  test("Render description", () => {});
+  test("Render icon", () => {});
+  test("Render pro-tip", () => {});
+});
