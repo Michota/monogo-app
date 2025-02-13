2d878f74e6f2123933a7f132b9b983ff116bdf81
michota <dawidmichota@gmail.com>
2025-02-13 17:09:22 +0100

    feat: convert Form styles  to css module

--- src/views/components/SentimentForm.tsx
+++ src/views/components/SentimentForm.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
+import styles from "./SentimentForm.module.css";
 
 interface SentimentFormProps {
   isAnalysisResultLoading: boolean;
@@ -10,7 +11,7 @@ interface SentimentFormProps {
 
 function SentimentForm({ isAnalysisResultLoading, text, setText, onInitializeAnalysis }: SentimentFormProps) {
   return (
-    <div style={{ display: "flex" }}>
+    <div className={styles.container}>
       <Input disabled={isAnalysisResultLoading} value={text} onChange={(e) => setText(e.target.value)} />
       <Button onClick={onInitializeAnalysis} isLoading={isAnalysisResultLoading}>
         Analizuj
