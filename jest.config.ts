8f6eb7aa845144f21a856c8e9acc50f79acc5712
michota <dawidmichota@gmail.com>
2025-02-13 17:38:39 +0100

    fix minor

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,6 @@ const config: Config = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\DawidMichota\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
