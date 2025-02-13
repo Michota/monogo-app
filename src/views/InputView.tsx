65a1bbfacce09a32c7bfa06bb8127eceae8ffa38
michota <dawidmichota@gmail.com>
2025-02-13 16:01:50 +0100

    feat: update InputView

--- src/views/InputView.tsx
+++ src/views/InputView.tsx
@@ -1,18 +1,39 @@
+import Modal from "@/components/Modal";
+import SentimentDescription from "@/components/SentimentDescription";
 import { useSentimentAnalysis } from "@/hooks/useSentimentAnalysis";
+import { getDominantSentiment } from "@/utils/getDominantSentiment";
+import { useState } from "react";
+import SentimentForm from "./components/SentimentForm";
 
 function InputView() {
-  const { sentiment, analysisResult, setSentiment, analyze, statuses } = useSentimentAnalysis();
+  const { text, analysisResult, setText, analyze, statuses } = useSentimentAnalysis();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { isLoading } = statuses;
+
+  const handleAnalyze = async () => {
+    const analysisResult = await analyze();
+
+    if (analysisResult.isError) {
+      return;
+    }
+
+    setIsModalOpen(true);
+  };
+
   return (
     <>
-      <input value={sentiment} onChange={(e) => setSentiment(e.target.value)} />
-      <button style={{ opacity: statuses.isLoading ? 0.3 : 1 }} onClick={analyze}>
-        analyze
-      </button>
-      {analysisResult?.map((result) => (
-        <p style={{ background: "red" }} key={result.label}>
-          {result.score}
-        </p>
-      ))}
+      <SentimentForm
+        isAnalysisResultLoading={isLoading}
+        onInitializeAnalysis={handleAnalyze}
+        setText={setText}
+        text={text}
+      />
+      {analysisResult && (
+        <Modal isModalOpen={isModalOpen} onModalClose={() => setIsModalOpen(false)}>
+          <SentimentDescription data={getDominantSentiment(analysisResult)} />
+        </Modal>
+      )}
     </>
   );
 }
